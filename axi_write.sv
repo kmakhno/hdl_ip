e00f68bd03b56bad3a666433deee5b2fd16b5867
Kostiantyn Makhno <kostiantynmakhno@gmail.com>
2021-08-20 18:18:55 +0300

    Changed posedge to negedge

--- axi_write.sv
+++ axi_write.sv
@@ -1,5 +1,5 @@
 task axi_write;
-input logic [3:0]  addr;
+input logic [31:0]  addr;
 input logic [31:0] data;
 input logic [3:0]  strb;
 logic [1:0]  resp;
@@ -12,8 +12,10 @@ begin
             s0_axi_awvalid = 1;
             s0_axi_awaddr = addr;
             while (!s0_axi_awready) begin
-                @(posedge s0_axi_aclk);
+                @(negedge s0_axi_aclk);
             end
+            @(posedge s0_axi_aclk);
+            s0_axi_awvalid = 0;
             //-------------------------
         end
 
@@ -23,20 +25,21 @@ begin
             s0_axi_wstrb = strb;
             s0_axi_wdata = data;
             while (!s0_axi_wready) begin
-                @(posedge s0_axi_aclk);
+                @(negedge s0_axi_aclk);
             end
+            @(posedge s0_axi_aclk);
+            s0_axi_wvalid = 0;
             //----------------------
         end
     join
     //--- Write response channel
     while (!s0_axi_bvalid) begin
-        @(posedge s0_axi_aclk);
+        @(negedge s0_axi_aclk);
     end
+    @(posedge s0_axi_aclk);
     s0_axi_bready = 1;
     @(posedge s0_axi_aclk);
     resp = s0_axi_bresp;
-    s0_axi_awvalid = 0;
-    s0_axi_wvalid = 0;
     s0_axi_bready = 0;
     $display("resp=%0d", resp);
 end
